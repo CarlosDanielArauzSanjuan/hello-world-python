04af816ed71728961a3a49e858495c3df9c21db7
CarlosDanielArauzSanjuan <186082333+CarlosDanielArauzSanjuan@users.noreply.github.com>
2024-10-29 12:29:04 -0500

    type of variables python (with recommendations)

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-#input
-name = input ("enter your name: C")
-#process
-message = f"helo world ¡¡ {name}"
-#output
-print(message)
+fullName = "Carlos Daniel Arauz Sanjuan" # string (you declare strings character with ""
+gender = "M" # Char
+age = 25 # Int
+height = 1,75 # Double
+trainer = False # Boolean
+
+#SETS
+
+hobbies = ["ver peliculas", 'leer libros', """motear, tocar instrumentos"""] # Array (matriz)
+Address = (dict) ["country": "colombia", "state":"santander" ] #Object
+# = type character comments 
+
+
+print (fullName, age, gender)
+print (f"""
+       student's Name: {fullName} 
+       student's Age: {age}
+       student's Suma: '{2+2}'
+""")
+
+
+#  RECOMMENDATIONS 
+
+#create variables with letters and numbers
+
+#the name of the variables must be without numbers // , 
+# //just in case you've to used numbers, you can add the number with a "_" before the number.
+# //example : name_1
+
+#If the variable's name is a phrase or many words must be the first word in lowercase, 
+# //and the other ones start in uppercase, example : firstNameUser
+
+#double quotes are for simplify if i need to organize more my code, Line 14-19
+
+# Alt + arrowpointers (up, down) are to move the codelines to other place
+
+# Alt + click to select many diferent lines for write and same time
