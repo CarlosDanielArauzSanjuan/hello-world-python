938b220cf2f830964c44d3ca089ef5b894b77745
CarlosDanielArauzSanjuan <186082333+CarlosDanielArauzSanjuan@users.noreply.github.com>
2024-10-29 12:29:58 -0500

    command type to convert datas

--- main.py
+++ main.py
@@ -1,37 +1,11 @@
-fullName = "Carlos Daniel Arauz Sanjuan" # string (you declare strings character with ""
-gender = "M" # Char
-age = 25 # Int
-height = 1,75 # Double
-trainer = False # Boolean
+fullName = input("Enter yout full name:")
+age = int(input("Enter your Age: ")) #command int( before input, convert the data string to int
+gender = chr(input("Enter your gender M, F, X :")) #command chr( before input, convert the data string to chr
+height = float(input("Enter your height: ")) #command float( before input, convert the data string to float
 
-#SETS
+print(type(fullName))  #command type shows the nature of the variable
+print(type(age)) 
 
-hobbies = ["ver peliculas", 'leer libros', """motear, tocar instrumentos"""] # Array (matriz)
-Address = (dict) ["country": "colombia", "state":"santander" ] #Object
-# = type character comments 
-
-
-print (fullName, age, gender)
-print (f"""
-       student's Name: {fullName} 
-       student's Age: {age}
-       student's Suma: '{2+2}'
-""")
-
-
-#  RECOMMENDATIONS 
-
-#create variables with letters and numbers
-
-#the name of the variables must be without numbers // , 
-# //just in case you've to used numbers, you can add the number with a "_" before the number.
-# //example : name_1
-
-#If the variable's name is a phrase or many words must be the first word in lowercase, 
-# //and the other ones start in uppercase, example : firstNameUser
-
-#double quotes are for simplify if i need to organize more my code, Line 14-19
-
-# Alt + arrowpointers (up, down) are to move the codelines to other place
-
-# Alt + click to select many diferent lines for write and same time
+print(f""" Hi user {fullName}. How are you?, I'm Python.
+      thanks por share your Age {age}, I didn't idea that you human gender are {gender}.
+      """)
